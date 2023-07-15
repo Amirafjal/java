85dbc9738d80bb8f9307bf68be882ef1f553ff20
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-07-16 00:30:24 +0530

    A2J pattern

---
+++ java/src/pattern/A2J.java
@@ -0,0 +1,98 @@
+package pattern;
+
+public class A2J {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=9;
+		for(int i = 0 ; i<n ; i++) {
+			for(int j=0; j<n; j++) {
+				if(i==0 && j>0 && j<(n-1)/2 || 
+					i == (n-1)/2 && j<=(n-1)/2 ||
+					j==0 && i>0 || 
+					j==(n-1)/2 && i>0) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++) {
+				if(j==0 || j==(n-1)/2 && i!=0 && i!=(n-1)/2 && i!=n-1 || 
+						i==0 && j<(n-1)/2 || i == (n-1)/2 && j<(n-1)/2 ||
+						i==n-1 && j<(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++) {
+				if(i == 0 && j>0 && j<= (n-1)/2 ||
+						i==n-1 && j>0 && j<=(n-1)/2 ||
+						j==0 && i>0 && i<n-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(j==0 || j==(n-1)/2 && i>0 && i<n-1 ||
+						i == 0 && j<(n-1)/2 || i==n-1 && j<(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(j==0 || i==0 && j<=(n-1)/2 || i==(n-1)/2 && j<=(n-1)/2 ||
+						i==n-1 && j<=(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(j==0 || i==0 && j<=(n-1)/2 || i==(n-1)/2 && j<(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(j==0 && i>0 && i<n-1 || i==0 && j>0 && j<(n-1)/2 || 
+						i==n-1 && j>0 && j<=(n-1)/4 || j==(n-1)/4 && i>=(n-1)/2 ||
+						i==(n-1)/2 && j>=(n-1)/4 && j<=(n-1)/2 ||
+						j==(n-1)/2 && i>=(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(j==0 || j==(n-1)/2 || i==(n-1)/2 && j<=(n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(i==0 && j<=(n-1)/2 ||i == n-1 && j<=(n-1)/2 ||
+						j==(n-1)/4 ) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n;j++) {
+				if(i==0 && j<=(n-1)/2 || j==(n-1)/4 && i<n-1 || i==n-1 && j>0 && j<(n-1)/4 
+						|| j==0 && i>=(n-1)/2 && i!=n-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
