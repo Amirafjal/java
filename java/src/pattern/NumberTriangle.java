598ddde949f9614f2040b9f125b339eabfa55d63
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-07-27 01:24:27 +0530

    zero one triangle

--- java/src/pattern/NumberTriangle.java
+++ java/src/pattern/NumberTriangle.java
@@ -5,14 +5,47 @@ public class NumberTriangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 5;
-		for(int i=1; i<n; i++) {
+		for(int i=1; i<n; i++) {	
+			//Number changing pyramid
+//			for(int j=1; j<=i; j++) {
+//				if(i==1) {
+//					System.out.print(j+" ");
+//				}else if(i==2){
+//					System.out.print(j+n-4+"  ");
+//				}else if(i==3){
+//					System.out.print(j+n-2+"  ");
+//				}else {
+//					System.out.print(j+n+1+"  ");
+//				}
+//			}
+			
+//			<--  Zero one triangle    -->
 			for(int j=1; j<=i; j++) {
-				if(true) {
+				if(i==1) {
 					System.out.print(j+" ");
-				}else {
-					System.out.print("  ");
+				}else if(i==2){
+					System.out.print(j-1+" ");
+				} else if(i==3){
+					if(j==1) {
+						System.out.print(j+" ");
+					}else if(j==2) {
+						System.out.print(j-2+" ");
+					}else if(j==3){
+						System.out.print(j-2+" ");
+					}
+				}else if(i==4){
+					if(j==1) {
+						System.out.print(j-1+" ");
+					}else if(j==2) {
+						System.out.print(j-1+" ");
+					}else if(j==3) {
+						System.out.print(j-3+" ");
+					}else {
+						System.out.print(j-3+" ");
+					}
 				}
 			}
+			
 			System.out.println();
 		}
 	}
